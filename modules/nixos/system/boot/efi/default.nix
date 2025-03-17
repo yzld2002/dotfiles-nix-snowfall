67bd32bf07b0e4fb3b193351cc864ca6f6a7b061
Zhichao <zhichao.lin@landing.ai>
2025-03-17 22:44:32 +0800

    fix

--- modules/nixos/system/boot/efi/default.nix
+++ modules/nixos/system/boot/efi/default.nix
@@ -13,11 +13,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.systemd-boot.configurationLimit = 5;
-    boot.loader.efi.canTouchEfiVariables = true;
-
-    # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
-    boot.loader.systemd-boot.editor = false;
+    boot.loader.grub = {
+      # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+      # devices = [ ];
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
   };
 }
--- systems/x86_64-linux/chopin-nix/default.nix
+++ systems/x86_64-linux/chopin-nix/default.nix
@@ -1,5 +1,6 @@
-{pkgs, ...}: {
+{pkgs, modulesPath, ...}: {
   imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
     ./hardware.nix
     ./disk.nix
   ];
@@ -9,6 +10,12 @@
 
   environment.systemPackages = with pkgs; [
     # Any particular packages only for this host
+    fastfetch
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    # change this to your ssh key
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3dHygJrJv/ZqC1Y8NhpghQK4hMTuacdJhcXtvfG7dN yzld2002@gmail.com"
   ];
 
   # ======================== DO NOT CHANGE THIS ========================
