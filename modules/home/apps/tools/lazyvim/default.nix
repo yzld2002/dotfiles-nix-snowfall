7700658ceab42a3278249259f37781d00555cb47
yzld2002 <yzld2002@gmail.com>
2025-03-21 11:15:56 +0800

    tools update

---
+++ modules/home/apps/tools/lazyvim/default.nix
@@ -0,0 +1,11 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.custom;
+let cfg = config.apps.tools.lazyvim;
+in {
+  options.apps.tools.lazyvim = with types; {
+    enable = mkBoolOpt false "Enable lazyvim";
+  };
+
+  config = mkIf cfg.enable { home.packages = [ pkgs.custom.lazyvim ]; };
+}
