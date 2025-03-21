e1b21af066ad0086353b08d42c687aa0e8fc4860
yzld2002 <yzld2002@gmail.com>
2025-03-21 14:45:56 +0800

    enable lazyvin

--- homes/x86_64-linux/yzld2002@chopin-nix/default.nix
+++ homes/x86_64-linux/yzld2002@chopin-nix/default.nix
@@ -1,14 +1,6 @@
-{
-  inputs,
-  pkgs,
-  system,
-  lib,
-  ...
-}: {
-    home.packages = with pkgs; [
-      fastfetch
-      btop
-    ];
-    apps.misc.enable = true;
-    apps.tools.direnv.enable = true;
+{ inputs, pkgs, system, lib, ... }: {
+  home.packages = with pkgs; [ fastfetch btop ];
+  apps.misc.enable = true;
+  apps.tools.direnv.enable = true;
+  apps.tools.lazyvim.enable = true;
 }
