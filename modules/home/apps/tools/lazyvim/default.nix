b94f0aea1ed267cb28ef1b15bd2256593a0b9bd8
yzld2002 <yzld2002@gmail.com>
2025-03-22 22:58:48 +0800

    test lazyvim

--- modules/home/apps/tools/lazyvim/default.nix
+++ modules/home/apps/tools/lazyvim/default.nix
@@ -1,11 +1,17 @@
-{ options, config, lib, pkgs, ... }:
+{ options, config, lib, pkgs, inputs, ... }:
 with lib;
 with lib.custom;
-let cfg = config.apps.tools.lazyvim;
+let 
+  cfg = config.apps.tools.lazyvim;
+  lazyvimFlake = inputs.self.inputs.root.outPath + "/packages/lazyvim";
 in {
   options.apps.tools.lazyvim = with types; {
     enable = mkBoolOpt false "Enable lazyvim";
   };
 
-  config = mkIf cfg.enable { home.packages = [ pkgs.nvim ]; };
+  config = mkIf cfg.enable { 
+    home.packages = [ 
+      (import lazyvimFlake).packages.${pkgs.system}.nvim
+    ]; 
+  };
 }
