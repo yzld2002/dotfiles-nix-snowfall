658d6925b156a42922fdb32c3ea4df8204c896bc
yzld2002 <yzld2002@gmail.com>
2025-03-23 15:21:17 +0800

    change to static IP

--- systems/x86_64-linux/chopin-nix/default.nix
+++ systems/x86_64-linux/chopin-nix/default.nix
@@ -1,14 +1,18 @@
-{pkgs, modulesPath, ...}: {
-  imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
-    ./hardware.nix
-    ./disk.nix
-  ];
+{ pkgs, modulesPath, ... }: {
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix") ./hardware.nix ./disk.nix ];
 
   # Enable Bootloader
   system.boot.efi.enable = true;
 
   suites.server.enable = true;
+
+  networking.interfaces.ens18.useDHCP = false;
+  networking.interfaces.ens18.ipv4.routes = [{
+    address = "192.168.50.21";
+    prefixLength = 24;
+    via = "192.168.50.1";
+  }];
 
   # ======================== DO NOT CHANGE THIS ========================
   system.stateVersion = "24.05";
--- systems/x86_64-linux/chopin-nix/hardware.nix
+++ systems/x86_64-linux/chopin-nix/hardware.nix
@@ -4,11 +4,10 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
