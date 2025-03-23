5747bdab34df99462a04e6079f21158568318f07
yzld2002 <yzld2002@gmail.com>
2025-03-23 15:04:31 +0800

    nit

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -1,12 +1,7 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ options, config, pkgs, lib, ... }:
 with lib;
-with lib.custom; let
+with lib.custom;
+let
   cfg = config.user;
   defaultIconFileName = "profile.png";
   defaultIcon = pkgs.stdenvNoCC.mkDerivation {
@@ -19,49 +14,49 @@ with lib.custom; let
       cp $src $out
     '';
 
-    passthru = {fileName = defaultIconFileName;};
+    passthru = { fileName = defaultIconFileName; };
   };
-  propagatedIcon =
-    pkgs.runCommandNoCC "propagated-icon"
-    {passthru = {inherit (cfg.icon) fileName;};}
-    ''
-      local target="$out/share/icons/user/${cfg.name}"
-      mkdir -p "$target"
+  propagatedIcon = pkgs.runCommandNoCC "propagated-icon" {
+    passthru = { inherit (cfg.icon) fileName; };
+  } ''
+    local target="$out/share/icons/user/${cfg.name}"
+    mkdir -p "$target"
 
-      cp ${cfg.icon} "$target/${cfg.icon.fileName}"
-    '';
+    cp ${cfg.icon} "$target/${cfg.icon.fileName}"
+  '';
 in {
   options.user = with types; {
     name = mkOpt str "yzld2002" "The name to use for the user account.";
-    initialPassword =
-      mkOpt str "yzld2002"
+    initialPassword = mkOpt str "yzld2002"
       "The initial password to use when the user is first created.";
-    icon =
-      mkOpt (nullOr package) defaultIcon
+    icon = mkOpt (nullOr package) defaultIcon
       "The profile picture to use for the user.";
-    extraGroups = mkOpt (listOf str) [] "Groups for the user to be assigned.";
-    extraOptions =
-      mkOpt attrs {}
+    extraGroups = mkOpt (listOf str) [ ] "Groups for the user to be assigned.";
+    extraOptions = mkOpt attrs { }
       "Extra options passed to <option>users.users.<name></option>.";
   };
 
   config = {
-    environment.systemPackages = with pkgs; [
-      propagatedIcon
-    ];
+    environment.systemPackages = with pkgs; [ propagatedIcon ];
 
     environment.sessionVariables.FLAKE = "/home/yzld2002/dotfiles-nix-snowfall";
 
-    users.users.${cfg.name} =
-      {
-        isNormalUser = true;
-        inherit (cfg) name initialPassword;
-        home = "/home/${cfg.name}";
-        group = "users";
+    users.users.${cfg.name} = {
+      isNormalUser = true;
+      inherit (cfg) name initialPassword;
+      home = "/home/${cfg.name}";
+      group = "users";
 
-        extraGroups =
-          ["wheel" "audio" "sound" "video" "networkmanager" "input" "tty" "docker"]
-          ++ cfg.extraGroups;
-      };
+      extraGroups = [
+        "wheel"
+        "audio"
+        "sound"
+        "video"
+        "networkmanager"
+        "input"
+        "tty"
+        "docker"
+      ] ++ cfg.extraGroups;
+    };
   };
 }
