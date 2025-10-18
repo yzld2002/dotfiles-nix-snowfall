c6a8aa57ab461949639f7359d4e3b25197bd7fd2
yzld2002 <yzld2002@gmail.com>
2025-10-18 17:31:15 +0800

    add terminal and lazygit, setup some keymaps for fzf-lua

--- modules/home/apps/editor/nvf/default.nix
+++ modules/home/apps/editor/nvf/default.nix
@@ -1,7 +1,17 @@
 { options, config, lib, pkgs, inputs, ... }:
 with lib;
 with lib.custom;
-let cfg = config.apps.editor.nvf;
+let
+cfg = config.apps.editor.nvf;
+fzfLuaKeymap = [
+  { key = "<leader>ff"; mode = ["n"]; action = ":FzfLua files<CR>"; desc = "files"; }
+  { key = "<leader>fg"; mode = ["n"]; action = ":FzfLua git_files<CR>"; desc = "git files"; }
+  { key = "<leader>fb"; mode = ["n"]; action = ":FzfLua buffers<CR>"; desc = "buffers"; }
+  { key = "<leader>sg"; mode = ["n"]; action = ":FzfLua grep<CR>"; desc = "Pick - Live Grep"; }
+  { key = "<leader>sw"; mode = ["n"]; action = ":FzfLua grep_cword<CR>"; desc = "Pick - Grep word under cursor"; }
+  { key = "<leader>sw"; mode = ["v"]; action = ":FzfLua grep_cword<CR>"; desc = "Pick - Grep word under cursor"; }
+  { key = "<leader>sr"; mode = ["n"]; action = ":FzfLua resume<CR>"; desc = "Resume"; }
+];
 in {
   options.apps.editor.nvf = with types; {
     enable = mkBoolOpt false "Enable nvf based neovim";
@@ -18,6 +28,21 @@ in {
         vim.lsp = {
           enable = true;
         };
+        vim.binds.whichKey = {
+          enable = true;
+        };
+        vim.fzf-lua = {
+          enable = true;
+          profile = "telescope";
+        };
+        vim.git.enable = true;
+        vim.terminal = {
+          toggleterm.enable = true;
+          toggleterm.lazygit.enable = true; 
+        };
+        
+        # keymaps
+        vim.keymaps = fzfLuaKeymap;
       };
     };
   };
