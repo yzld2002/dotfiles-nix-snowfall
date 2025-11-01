f96850f79768d8e68286bdf095348e11bfd20673
yzld2002 <yzld2002@gmail.com>
2025-11-01 10:10:26 +0800

    snacks

--- modules/home/apps/editor/nvf/keymaps/keymaps.nix
+++ modules/home/apps/editor/nvf/keymaps/keymaps.nix
@@ -2,7 +2,9 @@ let
   general = import ./general.nix;
   fzf = import ./fzf.nix;
   neotree = import ./neotree.nix;
+  snacks = import ./snacks.nix;
 in
   general
   ++ fzf
   ++ neotree
+  ++ snacks
