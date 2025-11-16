f7d1cd71751414773cd6ceff1ce26607a77ba0c9
yzld2002 <yzld2002@gmail.com>
2025-11-16 21:21:52 +0800

    remove snacks

--- modules/home/apps/editor/nvf/keymaps/keymaps.nix
+++ modules/home/apps/editor/nvf/keymaps/keymaps.nix
@@ -2,9 +2,7 @@ let
   general = import ./general.nix;
   fzf = import ./fzf.nix;
   neotree = import ./neotree.nix;
-  snacks = import ./snacks.nix;
 in
   general
   ++ fzf
   ++ neotree
-  ++ snacks
--- modules/home/apps/editor/nvf/keymaps/snacks.nix
+++
@@ -1,8 +0,0 @@
-[
-  {
-    key = "<c-/>";
-    mode = ["n"];
-    action = "function() Snacks.terminal(nil, { cwd = LazyVim.root() }) end";
-    desc = "terminal";
-  }
-]
