6b6c3d19ceb250c2f474c402cc0612be9208ff58
yzld2002 <yzld2002@gmail.com>
2025-11-20 21:53:00 +0800

    m600 changes

--- homes/x86_64-linux/yzld2002@m600-nix/default.nix
+++ homes/x86_64-linux/yzld2002@m600-nix/default.nix
@@ -7,10 +7,10 @@
 }: {
   apps.misc.enable = true;
   apps.shell.zsh.enable = true;
-  apps.tools.direnv.enable = true;
-  apps.tools.git.enable = true;
-  apps.tools.zellij.enable = true;
   apps.editor.lazyvimnix.enable = true;
   apps.browser.edge.enable = true;
   apps.terminal.ghostty.enable = true;
+  apps.tools.direnv.enable = true;
+  apps.tools.git.enable = true;
+  apps.tools.zellij.enable = true;
 }
